c7945317ff2f861ab0b116bce74a34c0de2072c9
CampeloGabriel1 <campelo1gabriel@gmail.com>
2026-01-29 00:14:30 -0300

    Add main.go to read and display memory information from /proc/meminfo
    - Implemented a simple Go program that reads the contents of /proc/meminfo.
    - Handles errors when reading the file and outputs the content to the console.

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 	
 func main () {
+	
 	content, err := os.ReadFile("/proc/meminfo")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Erro ao ler o arquivo: %v", err)
 		os.Exit(1)
 	}
-	fmt.Println(string(content))
+	texto := string(content)
+	linhas := strings.Split(texto, "\n")
+	var resultado []string
+	for _, linha := range linhas {
+		if strings.HasPrefix(linha, "MemTotal:") {
+			resultado = append(resultado, linha)
+		} else if strings.HasPrefix(linha, "MemAvailable:") {
+			resultado = append(resultado, linha)
+		}
+	}
+	for _, linha := range resultado {
+		fmt.Println(linha)
+	}
+
 }
