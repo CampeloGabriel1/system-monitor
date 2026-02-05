205a21c83169bf2e30c63c173588b47f04cf33bc
CampeloGabriel1 <campelo1gabriel@gmail.com>
2026-02-04 22:33:19 -0300

    Enhance memory and CPU display: add color coding for usage percentages

--- main.go
+++ main.go
@@ -5,16 +5,44 @@ import (
 	"time"
 )
 
+const (
+    Reset  = "\033[0m"
+    Red    = "\033[31m"
+    Yellow = "\033[33m"
+    Green  = "\033[32m"
+    Cyan   = "\033[36m"
+	Clear  = "\033[H\033[2J"
+)
+
+func getCor(uso float64) string {
+	if uso > 80 {
+		return Red
+	}
+	if uso > 50 {
+		return Yellow
+	}
+	return Green
+}
+
 func main() {
+	for {
+	fmt.Print(Clear)
+
+		fmt.Println(Cyan + "=== GO SYSTEM MONITOR (Pressione Ctrl+C para sair) ===" + Reset)
+		fmt.Println(time.Now().Format("15:04:05"))
+		fmt.Println("--------------------------------------------------")
+
 	// 1. Memória
 	mStats, err := GetMemoryStats()
 	if err != nil {
 		fmt.Printf("Erro memória: %v\n", err)
 	} else {
-		fmt.Printf("RAM: %.2f%% de %d kB usados\n", mStats.UsagePercentage(), mStats.Total)
+		usoMem := mStats.UsagePercentage()
+        corMem := getCor(usoMem) // Aplicando a cor na RAM também!
+        fmt.Printf("RAM: %s%.2f%%%s de %d kB usados\n", corMem, usoMem, Reset, mStats.Total)
 	}
 
-	// 2. CPU (Cálculo de Delta)
+	// 2. CPU 
 	statsA, _ := GetCPUStats()
 	time.Sleep(1 * time.Second)
 	statsB, _ := GetCPUStats()
@@ -23,5 +51,7 @@ func main() {
 	totalDelta := float64(statsB.Total - statsA.Total)
 	cpuUsage := (1.0 - idleDelta/totalDelta) * 100
 
-	fmt.Printf("CPU: %.2f%%\n", cpuUsage)
+	corCPU := getCor(cpuUsage)
+	fmt.Printf("CPU: %s%.2f%%%s\n", corCPU, cpuUsage, Reset)
+	}
 }
